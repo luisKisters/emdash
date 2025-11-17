869756fd9df9546414ee97ef615af0cfc28e88a7
arnestrickmann <115920878+arnestrickmann@users.noreply.github.com>
2025-11-17 15:13:32 -0800

    style: forma

--- src/renderer/components/ui/select.tsx
+++ src/renderer/components/ui/select.tsx
@@ -61,7 +61,7 @@ export const SelectContent = React.forwardRef<
     >
       <SelectPrimitive.Viewport
         className={cn(
-          'p-1 max-h-80 overflow-y-auto emd-select-viewport',
+          'emd-select-viewport max-h-80 overflow-y-auto p-1',
           position === 'popper' &&
             'h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]'
         )}
