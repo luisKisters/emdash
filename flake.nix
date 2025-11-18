ec7d759815a6a7bf79ddbb0f77af7c2333b2f851
Raban von Spiegel <rabanspiegel@gmail.com>
2025-11-17 21:17:29 -0800

    sha

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
               version = "0.3.29";
               src = cleanSrc;
               inherit nodejs;
-              npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+              npmDepsHash = "sha256-nkrk7yCq4R8tUasCTgiI9tAORBHV/E9v7CcRJpsbPOo=";
               npmBuildScript = "package:linux";
               nativeBuildInputs =
                 sharedEnv
