f90fb5df7d46b483dfc7fad239e0a484da78f271
moritzWa <wallawitsch@gmail.com>
2026-01-03 19:05:39 -0500

    ui: remove GitHub profile from sidebar footer
    Remove the GitHub profile/status display from the sidebar footer
    to save space. The GitHub connection is still used for cloning
    and other features, but the profile display was unnecessary.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1935,13 +1935,6 @@ const AppContent: React.FC = () => {
                     activeTask={activeTask || undefined}
                     onReorderProjects={handleReorderProjects}
                     onReorderProjectsFull={handleReorderProjectsFull}
-                    githubInstalled={ghInstalled}
-                    githubAuthenticated={isAuthenticated}
-                    githubUser={user}
-                    onGithubConnect={handleGithubConnect}
-                    githubLoading={githubLoading}
-                    githubStatusMessage={githubStatusMessage}
-                    githubInitialized={isGithubInitialized}
                     onSidebarContextChange={handleSidebarContextChange}
                     onCreateTaskForProject={handleStartCreateTaskFromSidebar}
                     isCreatingTask={isCreatingTask}
