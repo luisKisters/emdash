f46cf65d2cd8168ca790a8b7b2da0e09ea80a288
David Konopka <d@davidkonopka.de>
2026-04-12 23:10:18 +0200

    fix: update url

--- src/shared/app-identity.ts
+++ src/shared/app-identity.ts
@@ -3,4 +3,4 @@ export const PRODUCT_NAME = 'Emdash Beta';
 export const APP_NAME_LOWER = 'emdash-beta';
 export const UPDATE_CHANNEL = 'v1-beta';
 export const ARTIFACT_PREFIX = 'emdash-beta';
-export const R2_BASE_URL = 'https://releases.emdash.sh';
+export const R2_BASE_URL = 'https://pub-01ba393b9c5243cfa7243dac73dd03d6.r2.dev';
