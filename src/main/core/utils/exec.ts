d7a952428c94406f20a82543aa026ffced2ad53f
Jona Schwarz <jonaschwarz1@gmail.com>
2026-04-27 13:47:03 +0200

    fix: add prefix entry

--- src/main/core/utils/exec.ts
+++ src/main/core/utils/exec.ts
@@ -65,6 +65,8 @@ export async function addGitHubAuthConfig(
       '-c',
       'url.https://github.com/.insteadOf=git@github.com:',
       '-c',
+      'url.https://github.com/.insteadOf=ssh://git@github.com:',
+      '-c',
       'url.https://github.com/.insteadOf=ssh://git@github.com/'
     );
   }
