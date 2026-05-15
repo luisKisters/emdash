3a56d201b5f65135e9fdc1b9efd86c0fcdc76418
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2026-05-15 15:44:29 +0200

    chore(db): remove draft automation migrations

--- drizzle/0013_whole_scrambler.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `automation_runs` ADD `scheduled_at` integer;--> statement-breakpoint
-ALTER TABLE `automation_runs` ADD `worker_id` text;--> statement-breakpoint
-CREATE INDEX `idx_automation_runs_automation_scheduled` ON `automation_runs` (`automation_id`,`scheduled_at`);
--- drizzle/0014_automation_drafts.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `automations` ADD `is_draft` integer DEFAULT 0 NOT NULL;
