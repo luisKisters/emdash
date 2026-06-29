44eb09d71b87bedf2d6100e73c026736b272e648
David Konopka <d@davidkonopka.de>
2026-06-29 14:34:58 -0700

    fix: ui storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -17,6 +17,12 @@ const config: StorybookConfig = {
   viteFinal: (config) =>
     mergeConfig(config, {
       plugins: [vanillaExtractPlugin()],
+      optimizeDeps: {
+        include: [
+          '@vanilla-extract/sprinkles/createRuntimeSprinkles',
+          '@vanilla-extract/recipes/createRuntimeFn',
+        ],
+      },
       resolve: {
         alias: {
           '@': root,
